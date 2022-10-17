84bea2de3e25c6e9561852ad78b8c05be20f1720
oh <outputor777@gmail.com>
2022-10-17 19:14:00 +0900

    WebSockets 실시간 채팅 test

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
+const express = require('express');
 const http = require('http');
 const WebSocket = require('ws')
-const  express = require('express');
+const path = require('path');
+const { parse } = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('test'));
+app.get('/', (res, req) => req.send('test'));
 
-const handleListen = () => console.log(`Listening on ws://localhost:3000`)
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({server})
-server.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'anonymous'
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from Brower ❌'))
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg.toString('utf8'));
+    console.log(message);
+
+    // nickname을 설정해줘야 하는데 재실행이 계속 되서 그러는지 nickname이 반영이 안됨
+    if(message.type === 'new_message') {
+      console.log(socket.nickname);
+      sockets.forEach(aSocket => aSocket.send(`${socket['nickname']}: ${message.payload}`));
+    }else if (message.type ==='nickname') {
+      socket['nickname'] = message.payload;
+      console.log(socket.nickname)
+    }
+    // switch(message.type) {
+    //   case 'new_message':
+    //     sockets.forEach(aSocket => aSocket.send(`${socket['nickname']}: ${message.payload}`));
+    //   case 'nickname':
+    //     socket['nickname'] = message.payload;
+    //   }
+  })
+})
+
+const handleListen = () => console.log(`Listening on ws://localhost:5000`)
+server.listen(5000, handleListen);
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [message, setMessage] = useState('')
+  const [list, setList] = useState([]);
+
+  const socket = new WebSocket(`ws://localhost:5000`);
+
+  function makeMessage(type, payload) {
+    // 여기서 객체인데 type, payload 하나만 넣어주는 거 궁금함, 찾아보기
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+  }
+
+  socket.addEventListener('open', () => {
+    console.log('Connected to Server ✅')
+  })
+
+  socket.addEventListener('message', (message) => {
+    console.log('New message: ', message.data);
+    setMessage(message.data)
+  })
+
+  useEffect(() => {
+    if (message.length > 0) {
+      console.log(message);
+      setList((prev) => [...prev, message])
+    }
+  }, [message])
+
+  console.log('message', message)
+
+  socket.addEventListener('close', () => {
+    console.log('Disconnected from Server ❌');
+  })
+
+  function handleInput(event) {
+    const { value } = event.target;
+    setInput(value);
+  }
+
+  function handleNickInput(event) {
+    const { value } = event.target;
+    setNickname(value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    socket.send(makeMessage('new_message', input));
+    setInput('');
+  }
+
+
+  function handleNickSubmit(event) {
+    event.preventDefault();
+    socket.send(makeMessage('nickname', nickname));
+    setNickname('');
+  }
+
+
+  // setTimeout(() => {
+  //   socket.send('hello from the browser!');
+  // }, 3000)
+
   return (
-    <div>Zoom22222222222222222222222222222</div>
+    <div>
+      <header>
+        <h1>zoom</h1>
+      </header>
+      <main>
+        <form onSubmit={handleNickSubmit}>
+          <input
+            type='text'
+            placeholder='choose a nickname'
+            value={nickname}
+            onChange={handleNickInput}
+            required
+          />
+          <button>Save</button>
+        </form>
+        <ul>
+          {list.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='write a msg'
+            value={input}
+            onChange={handleInput}
+            required
+          />
+          <button>Send</button>
+        </form>
+      </main>
+    </div>
   )
 }
 
